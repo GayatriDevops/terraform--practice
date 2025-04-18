2c9afc60d3d27e8985a814cfed8c75681c334837
GayatriDevops <gayatrijyotsna.k@gmail.com>
2025-04-18 12:25:54 +0530

    deleted files

--- terraformday-3/output.tf
+++ terraformday-3/output.tf
@@ -1,10 +1,12 @@
 output "public-ip" {
     value = aws_instance.dev.public_ip
+    
   
 }
 
 output "private-ip" {
     value = aws_instance.dev.private_ip
+    sensitive = true
   
 }
 
--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "ami" {
-    description = "ami-id"
-    type = string
-    default = "ami-00a929b66ed6e0de6"
-  
-}
-
